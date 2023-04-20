74a93ac2932d3a37597b76af40d6f2e707b32792
BoilingSoup <boilingsoup@protonmail.com>
2023-04-19 20:28:39 -0700

    enable .env

--- netlify/functions/hello/hello.go
+++ netlify/functions/hello/hello.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	// fmt.Printf("%+v\n", request.Headers["cookie"])
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello, World!",
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: `{"test": "hello world"}`,
 	}, nil
 }
 
 func main() {
+	fmt.Println(os.Getenv("REPO_BULLETIN_TEST"))
+
 	lambda.Start(handler)
 }
